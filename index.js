95d694f83b37e3d86f40108db7623e3c4389cc5a
Katie West <katiewest820@gmail.com>
2017-10-12 09:51:28 -0700

    added_images_feature

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let food;
 let town;
 let value;
 let result = 0;
+let imgResult = 0;
+let photos;
+
 
 
 
@@ -29,31 +32,66 @@ console.log(value)
 
 function initialize (searchterm) {
   service = new google.maps.places.PlacesService($('.location, .food').get(0));
-
-service.textSearch({
-  query: searchterm
-  
-}, function(place){
-  console.log(place)
-  displayResults(place)
-});
+		service.textSearch({ query: searchterm }, function(place){
+  		console.log(place)
+  		getDetails(place[result])
+	})
 }
 
+ 
+function getDetails(place){
+	service.getDetails({ placeId: place.place_id }, function callback(placeId){
+		console.log(placeId)
+		photos = placeId.photos
+		displayImages()
+	});
+	
+	displayResults(place)
+	console.log(place)
+}
+
+function displayImages (){
+	clearResults()
+	$('.resultsImg').append(`<img src="${photos[imgResult++].getUrl({maxWidth: 200, maxHeight: 200})}">
+		<img src="${photos[imgResult++].getUrl({maxWidth: 300, maxHeight: 300})}">
+		<img src="${photos[imgResult++].getUrl({maxWidth: 200, maxHeight: 200})}">
+		<img src="${photos[imgResult++].getUrl({maxWidth: 200, maxHeight: 200})}">
+		<img src="${photos[imgResult++].getUrl({maxWidth: 200, maxHeight: 200})}">`)
+
+	
+	}
+
 function displayResults (place){
-	$('.results').html(`<p class="name">${place[result].name}</p>
-						<p class="address">${place[result].formatted_address}</p>`)
+	$('.results').html(`<p class="name">${place.name}</p>
+						<p class="address">${place.formatted_address}</p>`)
 	result++
 }
 
 function nextOption(){
 	$('.nextOption').on('click', function(){
-		console.log(result)
 		initialize(value)
+		imgResult = 0
 
 	})
 
 }
 
+function nextImg(){
+	$('.nextImg').on('click', function(){
+		displayImages()
+
+	})
+
+}
+
+function clearResults(){
+	$('.resultsImg').empty()
+
+}
+
+
+
 nextOption()
+nextImg()
 startPage()
 search()
