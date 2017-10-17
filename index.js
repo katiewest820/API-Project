44b9620b3e6a5478bf23de3a36e6153c3097ac2d
Katie West <katiewest820@gmail.com>
2017-10-17 12:48:24 -0700

    brewery_type_updates

--- index.js
+++ index.js
@@ -7,14 +7,17 @@ let result = 0;
 let imgResult = 0;
 let photos;
 let hours;
+let phone;
+let review;
+let reviewNum = 0;
 
 
 
 
-function startPage(){
+function lightboxOpen(){
 $('.beginButton').on('click', function(){
-	$('.firstPage').css('display', 'none')
-	$('.secondPage').css('display', 'block')
+	$('.beginButton').css('visibility', 'hidden');
+	$('.lightbox').fadeIn(200).css('display', 'block');
 })
 }
 
@@ -22,25 +25,30 @@ $('.beginButton').on('click', function(){
 function search(){
 $('.searchButton').on('click', function(event){
 	event.preventDefault();
-	food = $('.food').val();
+	food = $('.dropdown').val();
 	town = $('.location').val();
-	value = town + " " + food;
-initialize(value)
-console.log(value)
+	value = 'brewery ' + town + " " + food;
+initialize(value);
+console.log(value);
 
-$('.secondPage').css('display', 'none')
-$('.resultsPage').css('display', 'block')
+$('.firstPage').fadeOut(300);
+$('.lightbox').fadeOut(300);
+$('.loadScreen').css('display', 'block').delay(3000).fadeOut(400);
+setTimeout(function(){
+	$('.resultsPage').css('display', 'grid');
+}, 3000)
+
 
 })
 }
 
 
 
-
 function initialize (searchterm) {
   service = new google.maps.places.PlacesService($('.location, .food').get(0));
-		service.textSearch({ query: searchterm }, function(place){
+		service.textSearch({ query: searchterm, type: 'bar'}, function(place){
   		console.log(place)
+  		checkBeginningReset(place)
   		getDetails(place[result])
 	})
 }
@@ -51,32 +59,43 @@ function getDetails(place){
 		console.log(placeId)
 		photos = placeId.photos
 		hours = placeId.opening_hours
-		console.log(hours)
-
+		phone = placeId.formatted_phone_number
+		review = placeId.reviews
+		
+		console.log(placeId)
 		displayImages()
 		displayHours()
+		displayPhone()
+		reviews()
 	});
-	
 	displayResults(place)
-	console.log(place)
+}
 
+function checkBeginningReset(place){
+	if(place[result] == undefined){
+		result = 0
+	}
 }
 
 
 function displayImages (){
 	clearResults()
-	$('.resultsImg').append(`<img class="locationImg" src="${photos[imgResult++].getUrl({maxWidth: 400, maxHeight: 400})}">`)
+	$('.resultsImg').append(`<img class="locationImg" src="${photos[imgResult].getUrl({maxWidth: 400, maxHeight: 400})}">`)
 }
 
 function displayResults (place){
 	$('.results').html(`<h1 class="name">${place.name}</h1>
-						<p class="address">${place.formatted_address}</p>`)
-						
+						<address><a href="https://www.google.com/maps?q=${place.formatted_address}" target="_blank"><p class="address">${place.formatted_address}</p></a></address>`)
+
 	result++
 }
 
+function displayPhone(){
+	$('.results').append(`<p class="phone">${phone}</p>`)
+}
+
 function displayHours(){
-	$('.hours').html(`<ul>Hours
+	$('.hours').html(`<ul>
 	<li> ${hours.weekday_text[0]}</li>
 	<li> ${hours.weekday_text[1]}</li>
 	<li> ${hours.weekday_text[2]}</li>
@@ -85,20 +104,51 @@ function displayHours(){
 	<li> ${hours.weekday_text[5]}</li>
 	<li> ${hours.weekday_text[6]}</li>
 						</ul>`)
+}
 
+function reviews(){
+	$('.reviewSection').html(`<h2 class="rating">${review[reviewNum].rating} out of 5</h2>
+								<p class="review">${review[reviewNum].text}</p>`)
+	
+}
+
+function nextReview(){
+	$('.rightRev').on('click', function(){
+		reviewNum = reviewNum + 1;
+		
+		if(review[reviewNum] == undefined ){
+			 reviewNum = 0;
+		}
+		reviews()
+	})
+}
+
+function prevReview(){
+	$('.leftRev').on('click', function(){
+		reviewNum = reviewNum -1;
+		if(review[reviewNum] == undefined ){
+			reviewNum = 4;
+		}
+		reviews()
+	})
 }
 
 function nextOption(){
 	$('.nextOption').on('click', function(){
 		initialize(value)
-		imgResult = 0
+		imgResult = 0;
+		reviewNum = 0;
 
 	})
 
 }
 
 function nextImg(){
-	$('.fa-arrow-circle-right').on('click', function(){
+	$('.rightImg').on('click', function(){
+		imgResult = imgResult + 1;
+		if(photos[imgResult] == undefined){
+			imgResult = 0
+	}
 		displayImages()
 
 
@@ -107,8 +157,11 @@ function nextImg(){
 }
 
 function prevImg(){
-	$('.fa-arrow-circle-left').on('click', function(){
-		imgResult = imgResult -2
+	$('.leftImg').on('click', function(){
+		imgResult = imgResult -1;
+		if(photos[imgResult] == undefined){
+			imgResult = 9
+		}
 		displayImages()
 
 
@@ -117,11 +170,16 @@ function prevImg(){
 
 function clearResults(){
 	$('.resultsImg').empty()
-	
-	if(imgResult == 10){
-		return imgResult = 0
-	}return
 
+}
+
+function newSearch(){
+	$('.newSearch').on('click', function(){
+		 	$('.location').val('')
+		$('.resultsPage').css('display', 'none')
+		$('.firstPage').fadeIn(300).css('display', 'grid')
+		$('.lightbox').fadeIn(300).css('display', 'grid')
+	})
 }
 
 
@@ -129,5 +187,8 @@ function clearResults(){
 nextOption()
 nextImg()
 prevImg()
-startPage()
+lightboxOpen()
 search()
+nextReview()
+prevReview()
+newSearch()
